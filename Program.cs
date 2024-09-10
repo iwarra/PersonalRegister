8696622580649b281f5ef8e2d8a48c8b64c7adf0
Ivona Josipovic <josipovic.ivona@gmail.com>
2024-09-10 14:54:03 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿namespace PersonalRegister
+{
+
+    class Employee
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public decimal Salary { get; set; }
+
+        public Employee(string name, string lastName, decimal salary)
+        {
+            Name = name;
+            LastName = lastName;
+            Salary = salary;
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Delicious Bakery register! Please start be entering the number of employees: ");
+            int numberOfEmployees = int.Parse(Console.ReadLine());
+            //Crete a list for employees
+
+            for (int i = 0; i < numberOfEmployees; i++)
+            {
+                Console.WriteLine($"\nEnter details for employee {i + 1}:");
+
+                Console.Write("First Name: ");
+                string firstName = Console.ReadLine();
+
+                Console.Write("Last Name: ");
+                string lastName = Console.ReadLine();
+
+                Console.Write("Salary: ");
+                decimal salary = decimal.Parse(Console.ReadLine());
+
+                //create an Employee obj and add to the employees list
+            }
+
+            //Iterate over the employees list and show info for each one
+        }
+    }
+
+}
