b295a9b10ae1661999b74cdd1a0ee1182819c1a2
Ivona Josipovic <josipovic.ivona@gmail.com>
2024-09-12 15:16:57 +0200

    Separated the Employee class

---
+++ Employee.cs
@@ -0,0 +1,10 @@
+﻿
+namespace PersonalRegister
+{
+    internal class Employee(string name, string lastName, decimal salary)
+    {
+        public string Name { get; set; } = name;
+        public string LastName { get; set; } = lastName;
+        public decimal Salary { get; set; } = salary;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,6 @@
 ﻿namespace PersonalRegister
 {
 
-    public class Employee(string name, string lastName, decimal salary)
-    {
-        public string Name { get; set; } = name;
-        public string LastName { get; set; } = lastName;
-        public decimal Salary { get; set; } = salary;
-    }
-
     internal class Program
     {
         static void Main()
